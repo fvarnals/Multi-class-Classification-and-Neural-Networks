2f30d79064f62271086cb0f786035eea726505aa
fvarnals <f.varnals@gmail.com>
2019-02-08 20:50:16 +0000

    Update predict.m

--- predict.m
+++ predict.m
@@ -12,19 +12,19 @@ p = zeros(size(X, 1), 1);
 
 X = [ones(m,1) X]; % add 1s to X as first column for bias term
 
-hidden_inputs = X * Theta1'; % X is 5000 x 401, Theta1 = 25x401
+hidden_layer_inputs = X * Theta1'; % X is 5000 x 401, Theta1 = 25x401
 
-hidden_outputs = sigmoid(hidden_inputs); % sigmoid of resultant 5000x25
+hidden_layer_outputs = sigmoid(hidden_layer_inputs); % sigmoid of resultant 5000x25
 
-n = size(hidden_outputs, 1); % get n for size of 1s column needed next
+n = size(hidden_layer_outputs, 1); % get n for size of 1s column needed next
 
-hidden_outputs = [ones(n,1) hidden_outputs]; %add 1s column for bias
+hidden_layer_outputs = [ones(n,1) hidden_layer_outputs]; %add 1s column for bias
 
-output_inputs = hidden_outputs * Theta2'; % h_o = 5000x26, Theta2 = 10x26
+output_layer_inputs = hidden_layer_outputs * Theta2'; % h_o = 5000x26, Theta2 = 10x26
 
-output_outputs = sigmoid(output_inputs);
+output_layer_outputs = sigmoid(output_layer_inputs);
 
-[v p] = max(output_outputs, [], 2); % we have 5000x10 matrix so we want the
+[v p] = max(output_layer_outputs, [], 2); % we have 5000x10 matrix so we want the
                                     % max column value for each row to give #
 
 end
